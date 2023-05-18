743b33e3d31f1ec0227b06eb13123aee053ace63
Arth Patel <arthpatel.ce@gmail.com>
2023-05-18 18:35:49 +0530

    first

---
+++ 1-reverse a number.py
@@ -0,0 +1,11 @@
+"""
+This program will reverse any type of number you passed!
+"""
+
+def reverse_num(number):
+    return number[::-1]
+
+
+if __name__ == '__main__':
+    num = str(input("Enter the number:"))
+    print(f'Reversed Number of {num} is {reverse_num(num)}')
